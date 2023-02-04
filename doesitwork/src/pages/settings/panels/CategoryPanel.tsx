c6306906ce326a53369a6850805fff464acb0905
iTecX <matteovh@gmail.com>
2023-02-04 09:26:30 -0500

    Category management functional

--- doesitwork/src/pages/settings/panels/CategoryPanel.tsx
+++ doesitwork/src/pages/settings/panels/CategoryPanel.tsx
@@ -5,22 +5,164 @@ import {
     IconButton,
     Button,
     Chip,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    InputAdornment,
+    TextField,
+    Autocomplete,
+    DialogActions,
 } from "@mui/material";
 import { useConfirm } from "material-ui-confirm";
 import { useState, useEffect } from "react";
-import { MdCategory, MdLocationPin } from "react-icons/md";
+import { MdCategory } from "react-icons/md";
 import { del, get, post } from "../../../util/api";
 import { PanelWrapper } from "../SystemSettings";
 import { Category } from "../../../types";
 import * as MdIcons from "react-icons/md";
 import { useHorizontalScroll } from "../../../util/hscroll";
+import { IconSelector } from "../../../components/IconSelect/IconSelector";
+
+type CategoryEdit = { name: string; icon: string; flags: string[] };
+
+function CreateEditCategory(props: {
+    data: Partial<CategoryEdit> | null;
+    onClose: (data: CategoryEdit | null) => void;
+    mode: "Create" | "Edit";
+}) {
+    const [name, setName] = useState<string>("");
+    const [icon, setIcon] = useState<string>("");
+    const [flags, setFlags] = useState<string[]>([]);
+
+    function close(data: CategoryEdit | null) {
+        props.onClose(data);
+        setName("");
+        setIcon("");
+        setFlags([]);
+    }
+
+    useEffect(() => {
+        if (props.data) {
+            setName(props.data.name ?? "");
+            setIcon(props.data.icon ?? "");
+            setFlags(props.data.flags ?? []);
+        }
+    }, [props.data]);
+
+    return (
+        <Dialog
+            open={Boolean(props.data)}
+            onClose={() => close(null)}
+            className="create-edit-category-dialog"
+            fullWidth
+            maxWidth="sm"
+        >
+            <DialogTitle>{props.mode} Category</DialogTitle>
+            <DialogContent>
+                <Stack spacing={2} sx={{ marginTop: "8px" }}>
+                    <Stack spacing={1} direction="row">
+                        <IconSelector
+                            value={icon}
+                            onChange={setIcon}
+                            trigger={
+                                <Button
+                                    variant="contained"
+                                    startIcon={
+                                        (MdIcons as any)[icon] ? (
+                                            (MdIcons as any)[icon]({ size: 24 })
+                                        ) : (
+                                            <MdIcons.MdMiscellaneousServices
+                                                size={24}
+                                            />
+                                        )
+                                    }
+                                    sx={{
+                                        padding: "16px",
+                                        minWidth: "0px",
+                                        "& .MuiButton-startIcon": {
+                                            margin: "0px",
+                                        },
+                                    }}
+                                />
+                            }
+                        />
+                        <TextField
+                            label="Category Name"
+                            required
+                            placeholder="Laundry Machines"
+                            value={name}
+                            onChange={(event) => setName(event.target.value)}
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <MdIcons.MdEdit />
+                                    </InputAdornment>
+                                ),
+                            }}
+                            fullWidth
+                        />
+                    </Stack>
+                    <Autocomplete
+                        className="flag-input"
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Flags"
+                                InputProps={{
+                                    ...params.InputProps,
+                                    startAdornment: (
+                                        <>
+                                            <InputAdornment position="start">
+                                                <MdIcons.MdFlag
+                                                    className="adornment"
+                                                    size={24}
+                                                />
+                                            </InputAdornment>
+                                            {params.InputProps.startAdornment}
+                                        </>
+                                    ),
+                                }}
+                            />
+                        )}
+                        freeSolo
+                        options={[]}
+                        fullWidth
+                        multiple
+                        value={flags ?? []}
+                        onChange={(event, value) => setFlags(value ?? [])}
+                    />
+                </Stack>
+            </DialogContent>
+            <DialogActions>
+                <Stack spacing={1} direction="row">
+                    <Button
+                        startIcon={<MdIcons.MdCancel />}
+                        onClick={() => props.onClose(null)}
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        startIcon={<MdIcons.MdCheck />}
+                        onClick={() => {
+                            props.onClose({ name, icon, flags });
+                        }}
+                        variant="contained"
+                        disabled={name.length === 0}
+                    >
+                        Submit
+                    </Button>
+                </Stack>
+            </DialogActions>
+        </Dialog>
+    );
+}
 
 function CategoryItem(props: {
     category: Category;
     load: () => void;
     categories: Category[];
 }) {
-    const [editing, setEditing] = useState<boolean>(false);
+    const [editing, setEditing] = useState<CategoryEdit | null>(null);
     const confirm = useConfirm();
     const scrollRef = useHorizontalScroll(0.1);
 
@@ -38,7 +180,7 @@ function CategoryItem(props: {
                     className: "icon",
                 })
             ) : (
-                <MdLocationPin size={24} className="icon" />
+                <MdIcons.MdMiscellaneousServices size={24} className="icon" />
             )}
             <Typography className="name">{props.category.name}</Typography>
             {props.category.flags.length > 0 && (
@@ -51,7 +193,7 @@ function CategoryItem(props: {
                 </div>
             )}
             <Stack spacing={0.5} className="buttons" direction={"row"}>
-                <IconButton onClick={() => setEditing(true)}>
+                <IconButton onClick={() => setEditing({ ...props.category })}>
                     <MdIcons.MdEdit size={20} />
                 </IconButton>
                 <IconButton
@@ -60,6 +202,7 @@ function CategoryItem(props: {
                         confirm({
                             description: `This will permanently delete "${props.category.name}" and all services attached to it.`,
                         }).then(() => {
+                            console.log(props.category);
                             del<null>(
                                 `/categories/${props.category.category_id}`
                             ).then((result) => {
@@ -73,6 +216,23 @@ function CategoryItem(props: {
                     <MdIcons.MdDelete size={20} />
                 </IconButton>
             </Stack>
+            <CreateEditCategory
+                data={editing}
+                onClose={(data) => {
+                    setEditing(null);
+                    if (data) {
+                        post<string>(
+                            `/categories/${props.category.category_id}`,
+                            { data }
+                        ).then((result) => {
+                            if (result.success) {
+                                props.load();
+                            }
+                        });
+                    }
+                }}
+                mode="Edit"
+            />
         </Paper>
     );
 }
@@ -110,6 +270,20 @@ export function CategoryPanel() {
                     Add Category
                 </Button>
             </Stack>
+            <CreateEditCategory
+                data={creating ? {} : null}
+                onClose={(data) => {
+                    setCreating(false);
+                    if (data) {
+                        post<string>(`/categories`, { data }).then((result) => {
+                            if (result.success) {
+                                load();
+                            }
+                        });
+                    }
+                }}
+                mode="Create"
+            />
         </PanelWrapper>
     );
 }
--- server/controllers/category.py
+++ server/controllers/category.py
@@ -1,7 +1,14 @@
-from starlite import Controller, get, post, delete
+from starlite import Controller, get, post, delete, NotFoundException
 from _types import AppState, Category
 from deps import guard_isAdmin
+from typing import TypedDict
+from uuid import uuid4
 
+
+class CategoryCreateUpdate(TypedDict):
+    name: str
+    icon: str
+    flags: list[str]
 
 class CategoryController(Controller):
     path = "/categories"
@@ -9,3 +16,35 @@ class CategoryController(Controller):
     @get("", status_code=200)
     async def list_categories(self, app_state: AppState) -> list[Category]:
         return [{k: v for k, v in i.items() if k != "_id"} for i in app_state.database.categories.find({})]
+
+    @post("/{category_id:str}", status_code=200, guards=[guard_isAdmin])
+    async def update_category(self, app_state: AppState, category_id: str, data: CategoryCreateUpdate) -> str:
+        item: Category = app_state.database.categories.find_one(
+            {"category_id": category_id})
+        if item:
+            item["name"] = data["name"]
+            item["icon"] = data["icon"]
+            item["flags"] = data["flags"]
+            app_state.database.categories.replace_one(
+                {"category_id": category_id}, item, upsert=True)
+            return item["category_id"]
+        else:
+            raise NotFoundException(detail="Category not found")
+
+    @post("", status_code=201, guards=[guard_isAdmin])
+    async def create_category(self, app_state: AppState, data: CategoryCreateUpdate) -> str:
+        cid = uuid4().hex
+        item: Category = Category(
+            category_id=cid, name=data["name"], icon=data["icon"], flags=data["flags"])
+        app_state.database.categories.insert_one(item)
+        return cid
+
+    @delete("/{category_id:str}", status_code=204, guards=[guard_isAdmin])
+    async def delete_category(self, app_state: AppState, category_id: str) -> None:
+        item: Category = app_state.database.categories.find_one(
+            {"category_id": category_id})
+        if item:
+            app_state.database.categories.delete_one(
+                {"category_id": category_id})
+        else:
+            raise NotFoundException(detail="Category not found")
